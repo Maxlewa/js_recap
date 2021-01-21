5d21242d9531e68c6ff7bf282057e615636d6ba6
Maxence <lemairemaxence@hotmail.com>
2021-01-21 16:55:53 +0100

    modifs

--- public/js/fonction.js
+++ public/js/fonction.js
@@ -47,6 +47,36 @@
 // }
 // rand();
 
+// LOUISE
+
+let fonctionSaMere = () => {
+
+    let rand = Math.abs(Math.round(Math.random()*20))
+    console.log(rand);
+    let quest; 
+    let count = 0
+
+    do {
+        quest = prompt("Devinez un nombre entre 0 et 20")
+        if (Math.abs(quest-rand) == 5) {
+            alert("froid");
+        } else if (Math.abs(quest-rand) == 2) {
+            alert("chaud");
+        } else if (Math.abs(quest-rand) == 1) {
+            alert("brulant");
+        } else if (count == 5) {
+            alert("La partie est terminée")
+        } else if (quest == rand) {
+            alert('Tu as gagné')
+            break;
+        }
+        count++
+    } while (count <= 5 && quest!=rand);
+
+}
+
+fonctionSaMere()
+
 // EXO 2
 
 // let tab = [true, "alex", 3]
--- public/js/objet.js
+++ public/js/objet.js
@@ -1,130 +1,130 @@
-// OBJET
+// // OBJET
 
-// EXO 1
+// // EXO 1
 
-console.log("_____1_____");
+// console.log("_____1_____");
 
-let voiture = {
-    marque : "Audi",
-    modele : "A6",
-    type : "SUV",
-    color : "black",
-    prix : 20000,
-    phrase() {
-        console.log(`Ma ${this.marque} ${this.modele} est de type ${this.type}. Elle est ${this.color} et vaut ${this.prix} euros.`);
-    }
-}
+// let voiture = {
+//     marque : "Audi",
+//     modele : "A6",
+//     type : "SUV",
+//     color : "black",
+//     prix : 20000,
+//     phrase() {
+//         console.log(`Ma ${this.marque} ${this.modele} est de type ${this.type}. Elle est ${this.color} et vaut ${this.prix} euros.`);
+//     }
+// }
 
-voiture.phrase();
+// voiture.phrase();
 
-// EXO 2
+// // EXO 2
 
-console.log("_____2_____");
+// console.log("_____2_____");
 
-let personnage = {
-    nom : "Beyens",
-    prenom : "Tristan",
-    argent : 400,
-    sac : [],
-    acheter(x) {
-        if (this.argent >= x.prix) {
-            this.sac.push(article.nomProduit)
-            this.argent -= x.prix
-            x.stock--
-        } else {
-            console.log("Vous êtes trop pauvre ...");
-        }
-    }
-}
+// let personnage = {
+//     nom : "Beyens",
+//     prenom : "Tristan",
+//     argent : 400,
+//     sac : [],
+//     acheter(x) {
+//         if (this.argent >= x.prix) {
+//             this.sac.push(article.nomProduit)
+//             this.argent -= x.prix
+//             x.stock--
+//         } else {
+//             console.log("Vous êtes trop pauvre ...");
+//         }
+//     }
+// }
 
-let article = {
-    nomProduit : "montre",
-    prix : 200,
-    stock : 4,
-}
+// let article = {
+//     nomProduit : "montre",
+//     prix : 200,
+//     stock : 4,
+// }
 
-personnage.acheter(article);
-console.log(personnage);
-console.log(article);
+// personnage.acheter(article);
+// console.log(personnage);
+// console.log(article);
 
-// EXO 3
+// // EXO 3
 
-let molengeek = {
-    nEmployes: 0,
-    patron: {
-        nom: "Jefe",
-        prenom: "El",
-    },
-    employe: [],
-    addEmploye(x) {
-        if (this.employe.indexOf(x) > -1) {
-            console.log(`l'employé existe déjà`);
-        } else {
-            this.employe.push(x)
-            this.nEmployes++
-        }
-    },
-    delEmploye(x) {
-        if (this.employe.indexOf(x) > -1) {
-            this.employe.splice(x, 1);
-            this.nEmployes--
-        } else {
-            console.log("cet employé n'existe pas");
-        }
-    },
-    felicitations() {
-        let rando = Math.floor(Math.random()*this.employe.length);
-        console.log(rando);
-        console.log(`${this.employe[rando]} est l'employé du mois`);
-    }
-}
-molengeek.addEmploye("jeff");
-molengeek.addEmploye("louise");
-molengeek.addEmploye("alex");
-molengeek.addEmploye("maxence");
-molengeek.felicitations();
+// let molengeek = {
+//     nEmployes: 0,
+//     patron: {
+//         nom: "Jefe",
+//         prenom: "El",
+//     },
+//     employe: [],
+//     addEmploye(x) {
+//         if (this.employe.indexOf(x) > -1) {
+//             console.log(`l'employé existe déjà`);
+//         } else {
+//             this.employe.push(x)
+//             this.nEmployes++
+//         }
+//     },
+//     delEmploye(x) {
+//         if (this.employe.indexOf(x) > -1) {
+//             this.employe.splice(x, 1);
+//             this.nEmployes--
+//         } else {
+//             console.log("cet employé n'existe pas");
+//         }
+//     },
+//     felicitations() {
+//         let rando = Math.floor(Math.random()*this.employe.length);
+//         console.log(rando);
+//         console.log(`${this.employe[rando]} est l'employé du mois`);
+//     }
+// }
+// molengeek.addEmploye("jeff");
+// molengeek.addEmploye("louise");
+// molengeek.addEmploye("alex");
+// molengeek.addEmploye("maxence");
+// molengeek.felicitations();
 
-console.log(molengeek);
+// console.log(molengeek);
 
-// EXO 4
+// // EXO 4
 
-let perso = {
-    identite : {
-        nom : "Musk",
-        prenom : "Elon",
-    },
-    argent : 0,
-    lieu : "Bruxelles",
-    humeur : "good",
-    humeurChange() {
-        let rando = Math.floor(Math.random() * 2)
-        switch (rando) {
-            case 0:
-                this.humeur = "not good";
-                break;
-            case 1:
-                this.humeur = "très good";
-                break;
+// let perso = {
+//     identite : {
+//         nom : "Musk",
+//         prenom : "Elon",
+//     },
+//     argent : 0,
+//     lieu : "Bruxelles",
+//     humeur : "good",
+//     humeurChange() {
+//         let rando = Math.floor(Math.random() * 2)
+//         switch (rando) {
+//             case 0:
+//                 this.humeur = "not good";
+//                 break;
+//             case 1:
+//                 this.humeur = "très good";
+//                 break;
         
-            default:
-                break;
-        }
-    }
-}
+//             default:
+//                 break;
+//         }
+//     }
+// }
 
-let lieuChange = () => {
-    switch (x.lieu) {
-        case "Bruxelles":
-            x.lieu = "le travail"
-            break;
-        case "le travail":
-            x.lieu = "Bruxelles"
-            break;
-        default:
-            break;
-    }
-}
+// let lieuChange = () => {
+//     switch (x.lieu) {
+//         case "Bruxelles":
+//             x.lieu = "le travail"
+//             break;
+//         case "le travail":
+//             x.lieu = "Bruxelles"
+//             break;
+//         default:
+//             break;
+//     }
+// }
 
-let argentPlus = () => {
+// let argentPlus = () => {
 
-}
+// }
